3a16809f94fe35216d4b194f6d3304e030ebe404
sebax401 <cabrerasebastian867@gmail.com>
2026-06-05 16:32:58 -0400

    Se agrega la funcion de modificar los datos del vehiculo a escepcion del año, patente, marca y modelo

--- src/app/datos-vehiculo/datos-vehiculo.page.ts
+++ src/app/datos-vehiculo/datos-vehiculo.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, 
-          IonTitle, IonMenuButton, IonButton, IonButtons, IonToolbar, IonHeader, IonLabel } from '@ionic/angular/standalone';
+          IonTitle, IonMenuButton, IonButton, IonButtons, IonToolbar, IonHeader, IonLabel, IonItem, IonInput, IonAccordion, IonAccordionGroup } from '@ionic/angular/standalone';
 
 import { ActivatedRoute } from '@angular/router';
 
@@ -16,7 +16,7 @@ import { VehiculoService, Vehiculo } from '../services/vehiculo';
   styleUrls: ['./datos-vehiculo.page.scss'],
   standalone: true,
   imports: [IonContent, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonTitle, 
-            IonMenuButton, IonButton, IonButtons, IonToolbar, IonHeader, IonLabel, CommonModule, FormsModule, RouterLink ]
+            IonMenuButton, IonButton, IonButtons, IonToolbar, IonHeader, IonLabel, CommonModule, FormsModule, RouterLink, IonItem, IonInput, FormsModule, IonAccordion, IonAccordionGroup ]
 })
 export class DatosVehiculoPage implements OnInit {
 
@@ -51,19 +51,25 @@ export class DatosVehiculoPage implements OnInit {
       });
     }
 
-    modificarVehiculo() {
-      if (!this.vehiculo) return;
+  modificarVehiculo() {
+    if (!this.vehiculo) return;
 
-      this.vehiculoService.modificarVehiculo(this.vehiculo.idVehiculo, this.vehiculo).subscribe({
-        next: () => {
-          alert('Vehículo modificado correctamente');
-        },
-        error: (error) => {
-          console.error('Error al modificar:', error);
-          alert('Error al modificar vehículo');
-        }
-      });
-    }
+    this.vehiculo.kilometrajeActual = Number(this.vehiculo.kilometrajeActual);
+    this.vehiculo.proximoKilometraje = Number(this.vehiculo.proximoKilometraje);
+
+    this.vehiculoService.modificarVehiculo(
+      this.vehiculo.idVehiculo,
+      this.vehiculo
+    ).subscribe({
+      next: () => {
+        alert('Vehículo modificado correctamente');
+      },
+      error: (error) => {
+        console.error('Error al modificar:', error);
+        alert('Error al modificar vehículo');
+      }
+    });
+  }
 
   eliminarVehiculo() {
     if (!this.vehiculo) return;
